1f1ec61f8fc5e5d09bd2b89eda0e564698b2e7a5
Muhammad Abrar Argianata <114975445+muhammadabrarargianata@users.noreply.github.com>
2022-11-17 22:15:27 +0700

    Add files via upload
    push

---
+++ Muhammad Abrar Argianata-2170231013-Pemrograman Web-A1/Pertemuan4/modul 7/latihan1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variabel</title>
+</head>
+<body>
+<?php
+$nilai_1 = 10;
+$nilai_2 = 3;
+$nilai_3 = 2 * $nilai_1 + 8 * $nilai_2;
+echo "nilai = ", $nilai_3;
+echo "<br>";
+$jumlah = $nilai_1 + $nilai_2;
+echo "Hasil dari $nilai_1 + $nilai_2 adalah $jumlah"; echo "<br><br>";
+echo "\" Nama : Anrie Nur Rizky\" <br>";
+echo "NIM : 2170231012"; 
+?>
+</body>
+</html>
